74724cc07a627b5b439f311d042a7f059960d0b6
Nicolas Massardo <nmassardot@uc.cl>
2021-11-05 10:17:01 -0300

    feat(customers): add resource manager

--- plutto/core.py
+++ plutto/core.py
@@ -4,6 +4,8 @@ Core module to house the Plutto object of the Plutto Python SDK.
 
 from plutto.client import Client
 from plutto.constants import API_BASE_URL, API_VERSION
+from plutto.managers import CustomersManager
+from plutto.resources import customer
 from plutto.version import __version__
 
 class Plutto:
@@ -17,3 +19,16 @@ class Plutto:
             user_agent=f"plutto-python/{__version__}",
         )
 
+        self.__customers_manager = None
+
+    @property
+    def customers(self):
+        """Proxies the customers manager."""
+        if self.__customers_manager is None:
+            self.__customers_manager = CustomersManager("/customers", self._client)
+
+        return self.__customers_manager
+
+    @customers.setter
+    def customers(self, value):
+        raise NameError("Attribute name corresponds to a manager")
