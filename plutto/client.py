55b7d6c2734f845cd918b1fe192de74440231ed4
Nicolas Massardo <nmassardot@uc.cl>
2021-11-05 10:06:54 -0300

    feat(client): add request method

--- plutto/client.py
+++ plutto/client.py
@@ -2,6 +2,9 @@
 Module to house the Client object of the Plutto Python SDK.
 """
 
+from json.decoder import JSONDecodeError
+import httpx
+
 class Client:
     """Encapsulates the client behaviour and methods."""
 
@@ -12,3 +15,37 @@ class Client:
         self.user_agent = user_agent
         self.params = params
         self.__client = None
+
+    @property
+    def _client(self):
+        """Gets the client object."""
+        if self.__client is None:
+            self.__client = httpx.Client(
+                base_url=self.base_url,
+                headers=self.headers,
+                params=self.params,
+            )
+        return self.__client
+
+    @property
+    def headers(self):
+        """Gets the headers for the client."""
+        return {
+            "User-Agent": self.user_agent,
+            "Authorization": f"Bearer {self.api_key}",
+        }
+
+    def request(self, path, paginated=False, method="get", params=None, json=None):
+        """
+        Uses the internal httpx client to make a simple request.
+        """
+        if paginated:
+            pass
+
+        response = self._client.request(method, path, params=params, json=json)
+        response.raise_for_status()
+
+        try:
+            return response.json()
+        except JSONDecodeError:
+            return {}
