1514132c88c9e02b5df445dc0ac5e3dfd5fc7e20
Nicolas Massardo <nmassardot@uc.cl>
2021-11-15 12:47:03 -0300

    feat(customers): add permission custom method

---
+++ plutto/resources/customer_permission.py
@@ -0,0 +1,7 @@
+"""Module to hold the Permission resource."""
+
+from plutto.mixins.resource_mixin import ResourceMixin
+
+
+class CustomerPermission(ResourceMixin):
+    """Represents a Permission resource."""
