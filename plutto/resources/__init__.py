25d727e6ebcdf6d9cb3c4c9aedae84c40463eee9
Nicolas Massardo <nmassardot@uc.cl>
2021-11-16 11:42:44 -0300

    fix(resources): add customer permission to init

--- plutto/resources/__init__.py
+++ plutto/resources/__init__.py
@@ -1,4 +1,5 @@
 """Init file for the resources module of the SDK."""
 
 from .customer import Customer
+from .customer_permission import CustomerPermission
 from .generic_plutto_resource import GenericPluttoResource
