76507b9597d0838aa0e5e3d1e3da5667dfc46630
Nicolas Massardo <nmassardot@uc.cl>
2021-11-16 11:43:14 -0300

    test(core): fix customers test

--- tests/test_core.py
+++ tests/test_core.py
@@ -9,3 +9,21 @@ class TestCorePluttoObject:
         plutto = Plutto(api_key)
         assert isinstance(plutto._client, Client)
         assert isinstance(plutto.customers, ManagerMixin)
+
+    def test_customers_manager(self):
+        # pylint: disable=protected-access
+        api_key = "test_api_key"
+        plutto = Plutto(api_key)
+
+        assert plutto._Plutto__customers_manager is None
+        assert isinstance(plutto.customers, ManagerMixin)
+        assert plutto._Plutto__customers_manager is not None
+        assert isinstance(plutto._Plutto__customers_manager, ManagerMixin)
+
+        with pytest.raises(NameError):
+            plutto.customers = None
+
+        assert plutto.customers is not None
+        assert isinstance(plutto.customers, ManagerMixin)
+        assert isinstance(plutto._Plutto__customers_manager, ManagerMixin)
+
