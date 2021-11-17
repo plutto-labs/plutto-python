d8312a8dd49205bd8213c8cf44793e96b680e803
Nicolas Massardo <nmassardot@uc.cl>
2021-11-17 17:37:46 -0300

    feat(invoices): create manager

--- plutto/managers/__init__.py
+++ plutto/managers/__init__.py
@@ -1,5 +1,6 @@
 """Init file for the managers module of the SDK."""
 
 from .customers_manager import CustomersManager
+from .invoices_manager import InvoicesManager
 from .meter_events_manager import MeterEventsManager
 from .subscriptions_manager import SubscriptionsManager
