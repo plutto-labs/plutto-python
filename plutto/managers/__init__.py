e230476f35d8612b7422e7dc7dafaebd5f449649
Nicolas Massardo <nmassardot@uc.cl>
2021-11-17 13:52:06 -0300

    feat(meter-event): create manager

--- plutto/managers/__init__.py
+++ plutto/managers/__init__.py
@@ -1,4 +1,5 @@
 """Init file for the managers module of the SDK."""
 
 from .customers_manager import CustomersManager
+from .meter_events_manager import MeterEventsManager
 from .subscriptions_manager import SubscriptionsManager
