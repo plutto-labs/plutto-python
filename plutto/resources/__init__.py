88244fc32837720ae68d7d5bc9858688568096c9
Nicolas Massardo <nmassardot@uc.cl>
2021-11-17 13:52:18 -0300

    feat(meter-event): create resource

--- plutto/resources/__init__.py
+++ plutto/resources/__init__.py
@@ -3,4 +3,5 @@
 from .customer import Customer
 from .customer_permission import CustomerPermission
 from .generic_plutto_resource import GenericPluttoResource
+from .meter_event import MeterEventResource
 from .subscription import Subscription
---
+++ plutto/resources/meter_event.py
@@ -0,0 +1,7 @@
+"""Module to hold a meter event resource."""
+
+from plutto.mixins import ResourceMixin
+
+
+class MeterEventResource(ResourceMixin):
+    """Represents a Meter Event Resource."""
