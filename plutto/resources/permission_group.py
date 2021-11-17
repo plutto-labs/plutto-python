e1478295e075b8cccfad48e9da88b4d8af7178af
Nicolas Massardo <nmassardot@uc.cl>
2021-11-17 18:00:34 -0300

    feat(permission-group): create resource

---
+++ plutto/resources/permission_group.py
@@ -0,0 +1,7 @@
+"""Module to hold the permission group resource."""
+
+from plutto.mixins.resource_mixin import ResourceMixin
+
+
+class PermissionGroup(ResourceMixin):
+    """Represents a Permission Group resource."""
