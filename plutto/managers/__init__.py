7dc87f0e268f2e1e3e88f0fb1a780b8541cddfe4
Nicolas Massardo <nmassardot@uc.cl>
2021-11-17 17:59:01 -0300

    feat(products): create manager

--- plutto/managers/__init__.py
+++ plutto/managers/__init__.py
@@ -3,4 +3,5 @@
 from .customers_manager import CustomersManager
 from .invoices_manager import InvoicesManager
 from .meter_events_manager import MeterEventsManager
+from .products_manager import ProductsManager
 from .subscriptions_manager import SubscriptionsManager
